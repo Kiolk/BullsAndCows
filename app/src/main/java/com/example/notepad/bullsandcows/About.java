4d40b853b57405171674df64ba5273933c114623
Yauhen Slizh <tyteishi@gmail.com>
2017-11-19 12:39:27 +0300

    Add one small comment for example

--- app/src/main/java/com/example/notepad/bullsandcows/About.java
+++ app/src/main/java/com/example/notepad/bullsandcows/About.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class About extends AppCompatActivity {
-
+//comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
