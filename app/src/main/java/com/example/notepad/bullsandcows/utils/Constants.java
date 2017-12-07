9e10534f3df7b0abd67729c90c086c80de5921a5
Kiolk <tyteishi@gmail.com>
2017-12-07 08:20:24 +0300

    Generated UserDataBaseEndpoint submit on backend

--- app/src/main/java/com/example/notepad/bullsandcows/utils/Constants.java
+++ app/src/main/java/com/example/notepad/bullsandcows/utils/Constants.java
@@ -1,7 +1,7 @@
 package com.example.notepad.bullsandcows.utils;
 
 public class Constants {
-
+//comment
     public final static String DISCONNECT_SERVER = "Not possible connect to server";
     public final static String TAG = "MyLogs";
     public final static String FOLDER_DESTINATION_IN_SD = "Download";
